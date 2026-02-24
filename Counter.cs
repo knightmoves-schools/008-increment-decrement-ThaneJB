bb9e21a779b3fbd45e6824a35d8dedbf4200976b
Thane <thabay0606@students.maquoketaschools.org>
2026-02-24 14:18:33 -0600

    Did some minor enhancements.

--- Counter.cs
+++ Counter.cs
@@ -4,8 +4,18 @@ public class Counter
     public static int Visitors = 100;
 
     // Add your code here
-    public int PrefixIncrement(++Visitors);
-    public int PostfixIncrement(Visitors++);
-    public int PrefixDecrement(--Visitors);
-    public int PostfixDecrement(Visitors--);
+    public static int PrefixIncrement;
+    public static int PostfixIncrement;
+    public static int PrefixDecrement;
+    public static int PostfixDecrement;
+
+    static Counter()
+    {
+        PrefixIncrement = ++Visitors;
+        PostfixIncrement = Visitors++;
+        PrefixDecrement = --Visitors;
+        PostfixDecrement = Visitors--;
+
+        Visitors = 100;
+    }
 }
