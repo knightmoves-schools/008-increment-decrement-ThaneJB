d639ecb1d08e3630c5e42aa646dd46ccee44abc7
Thane <thabay0606@students.maquoketaschools.org>
2026-02-25 14:04:19 -0600

    Removed static.

--- Counter.cs
+++ Counter.cs
@@ -4,9 +4,9 @@ public class Counter
     public static int Visitors = 100;
 
     // Add your code here
-    public static int PrefixIncrement = (++Visitors);
-    public static int PostfixIncrement = (Visitors++);
-    public static int PrefixDecrement = (--Visitors);
-    public static int PostfixDecrement = (Visitors--);
+    public int PrefixIncrement = (++Visitors);
+    public int PostfixIncrement = (Visitors++);
+    public int PrefixDecrement = (--Visitors);
+    public int PostfixDecrement = (Visitors--);
 
 }
