6a770823e06fa7ce238b40a1ffdc7316ce368659
Thane <thabay0606@students.maquoketaschools.org>
2026-02-25 13:52:29 -0600

    Maybe...

--- Counter.cs
+++ Counter.cs
@@ -1,7 +1,7 @@
 ﻿namespace knightmoves;
 public class Counter
 {
-    public static int Visitors = 100;
+    
 
     // Add your code here
     public static int PrefixIncrement;
@@ -9,8 +9,9 @@ public class Counter
     public static int PrefixDecrement;
     public static int PostfixDecrement;
 
-    public void Count()
-    {
+    public void Count() {
+        int Visitors = 100;
+
         PrefixIncrement("Visitors", ++Visitors);
         PostfixIncrement("Visitors",Visitors++);
         PrefixDecrement("Visitors",--Visitors);
